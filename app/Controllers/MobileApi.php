dadc7e21517641c7a19b5c1e01a9ce1d5d9ccb9c
Awam Abdulnabi <101290214+awam97@users.noreply.github.com>
2026-08-16 13:43:25 +0200

    Refactor mobile controller to use the API service

---
+++ app/Controllers/MobileApi.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Libraries\MobileApiService;
+
+class MobileApi extends BaseController
+{
+    protected MobileApiService $api;
+
+    public function __construct()
+    {
+        $this->api = new MobileApiService();
+    }
+
+    public function login()
+    {
+        return $this->api->login();
+    }
+
+    public function me()
+    {
+        return $this->api->me();
+    }
+
+    public function cities()
+    {
+        return $this->api->cities();
+    }
+
+    public function activities()
+    {
+        return $this->api->activities();
+    }
+
+    public function activity(int $id)
+    {
+        return $this->api->activity($id);
+    }
+
+    public function myActivities()
+    {
+        return $this->api->myActivities();
+    }
+
+    public function enroll()
+    {
+        return $this->api->enroll();
+    }
+
+    public function unenroll()
+    {
+        return $this->api->unenroll();
+    }
+}
