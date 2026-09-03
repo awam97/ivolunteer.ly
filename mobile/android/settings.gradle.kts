83bc81029c3fa3cc291962bdf5d466507e2e71cc
Awam Abdulnabi <awam.abdelnabi1997@gmail.com>
2026-09-03 16:06:52 +0200

    Remove duplicate Google Services plugin

--- mobile/android/settings.gradle.kts
+++ mobile/android/settings.gradle.kts
@@ -20,7 +20,6 @@ pluginManagement {
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "9.0.1" apply false
-    id("com.google.gms.google-services") version "4.3.15" apply false
     // START: FlutterFire Configuration
     id("com.google.gms.google-services") version("4.3.15") apply false
     // END: FlutterFire Configuration
